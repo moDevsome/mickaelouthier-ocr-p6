a325f67b3c4fa306006499c679aa07d1bd78b958
moDevsome <contact@mickael-outhier.fr>
2022-12-02 15:30:43 +0100

    * Ajout du middleware express.json() qui interceptera toutes les requêtes avec un Content-Type = "application/json" pour les parser et les ajouter dans l'objet request en tant que propriété "body" * Ajout d'un bloc middleware permettant l'ajout d'en-têtes HTTP dans la réponse

--- src/api.js
+++ src/api.js
@@ -2,14 +2,32 @@
  * Point d'entrée de l'API
  */
 
-// Importe Express et créer une nouvelle instance de l'application
+// Importe Express et créer une nouvelle instance de l'API
 const express = require('express');
 const api = express(); // application Express
 
-api.get('/', (req, res) => {
+// On ajoute le middleware express.json() qui interceptera toutes les requêtes avec un Content-Type = "application/json"
+// pour les parser et les ajouter dans l'objet request en tant que propriété "body"
+api.use(express.json());
 
-    res.write('Hello les gens, API dispo !!');
+// Construction des entêtes HTTP
+api.use((request, response, next) => {
+
+    response.setHeader('Access-Control-Allow-Origin', '*'); // TODO:gestion du CORS
+    response.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    response.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+
+    next();
+
+});
+
+api.get('/', (request, res) => {
+
+    console.log(request);
+
+    res.write('Hello les gens, API !!');
     res.end();
+
 });
 
  module.exports = api;
