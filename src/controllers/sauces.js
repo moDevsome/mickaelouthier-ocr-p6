f31e60028ac5a5eb3063f47660e3c8d9c1b458ba
moDevsome <contact@mickael-outhier.fr>
2022-12-08 21:50:50 +0100

    * Développement de la méthode "POST /api/sauces"

--- src/controllers/sauces.js
+++ src/controllers/sauces.js
@@ -5,9 +5,40 @@
  * -----------------------------------------------------
  */
 
- const log = require('../consoleLog');
+const log = require('../consoleLog');
+const fs = require('fs');
+const os = require('os');
 
- const sauceModel = require('../models/sauce');
+const sauceModel = require('../models/sauce');
+const { resolve } = require('path');
+const { reject } = require('bcrypt/promises');
+
+/**
+ * Fonction interne permettant de filtrer le contenu de chaque données envoyées par le formulaire
+ * La fonction filtre chaque chaine de caractère et retourne l'objet
+ *
+ * @param sauceData objet contenant les données envoyées par le client
+ * @return object L'objet traité à travers une promesse
+*/
+const dataSanitize = (sauceData) => {
+
+    return new Promise((resolve) => {
+
+        const keys = Object.keys(sauceData);
+        let i = 0;
+        keys.forEach(key => {
+
+            let str = sauceData[key].toString();
+            sauceData[key] = str.replace(/(<([^>]+)>)/gi, '');
+            i++;
+
+            if(i === key.length) resolve(sauceData);
+
+        })
+
+    })
+
+}
 
 /**
  * GET /api/sauces
@@ -63,14 +94,79 @@ exports.getSauce = (request, response) => {
  * @param response La réponse Htpp
  * @return Response
 */
-// TODO:développer le corps de la méthode
-exports.postSauce = (request, response) => {
+exports.postSauce = async (request, response) => {
 
-    console.log(request.locals.userId);
+    // Parse le request body
+    let sauceData = {};
+    try {
 
-    return response.status(200).json({
-        message : 'La sauce a bien été ajoutée.'
-    });
+        sauceData = JSON.parse(request.body.sauce);
+
+    }
+    catch(error) {
+
+        log.output('Echec lors du parse du request body. '+ error.message);
+        return response.status(500).json({ error });
+
+    }
+
+    // Filtre les données
+    sauceData = await dataSanitize(sauceData);
+
+    // Vérifie l'existence de l'image et définie l'URL
+    const imageFilename = request.file.filename || 'NO-image';
+    const osSeparator = os.homedir().split('\\').length ? '\\' : '/';
+    const imageFilePath = process.cwd() + osSeparator +'images'+ osSeparator + imageFilename;
+    if(!fs.existsSync(imageFilePath)) {
+
+        log.output('Echec du téléchargement de l\'image.');
+        return response.status(500).json({
+            message: 'Echec du téléchargement de l\'image.'
+        });
+
+    }
+    else {
+
+        sauceData.imageUrl = request.protocol +'://'+ request.get('host') +'/images/'+ imageFilename;
+
+    }
+
+    // Enregistrement de la Sauce en base de données
+    const sauceEntity = sauceModel({
+        ...sauceData,
+        likes: 0,
+        dislikes: 0,
+        usersLiked: [],
+        usersDisliked: []
+    })
+
+    sauceEntity.save()
+        .then(() => {
+
+            log.output('=> Réussite de la création de la sauce en base de données', 'success');
+
+            return response.status(200).json(
+                { message: 'La sauce a bien été ajoutée. Merci pour votre participation.' }
+            )
+
+        })
+        .catch(saveError => {
+
+            log.output('=> Echec de la création de la sauce en base de données', 'error');
+
+            // On supprime l'image pour éviter une consommation illégitime de l'espace disque
+            fs.unlink(imageFilePath, (error) => {
+
+                if(error) log.output('Erreur à la suppression de l\'image.', 'error');
+                else log.output('L\'image a été supprimée.');
+
+                return response.status(500).json(
+                    { message: saveError }
+                )
+
+            });
+
+        });
 
 }
 
--- src/models/sauce.js
+++ src/models/sauce.js
@@ -9,17 +9,22 @@
  const mongooseUniqueValidator = require('mongoose-unique-validator');
 
  const sauceSchema = mongoose.Schema({
-    userId: { type: String, unique: true, required: true },
-    name: { type: String, required: true },
-    manufacturer: { type: String, required: true },
-    description: { type: String, required: true },
-    mainPepper: { type: String, required: true },
-    imageUrl: { type: String, required: true },
-    heat: { type: Number, required: true },
-    likes: { type: Number, required: true },
-    dislikes: { type: Number, required: true },
-    usersLiked: { type: [String], required: true },
-    usersDislike: { type: [String], required: true }
+      userId: { type: String, required: true },
+      name: { type: String, required: true },
+      manufacturer: { type: String, required: true },
+      description: { type: String, required: true },
+      mainPepper: { type: String, required: true },
+      imageUrl: { type: String, required: true },
+      heat: {
+         type: Number,
+         required: true,
+         min: [1, 'La valeur "heat" doit être comprise entre 1 et 10'],
+         max: [10, 'La valeur "heat" doit être comprise entre 1 et 10']
+      },
+      likes: { type: Number, required: true },
+      dislikes: { type: Number, required: true },
+      usersLiked: { type: [String], required: true },
+      usersDislike: { type: [String], required: true }
  });
  sauceSchema.plugin(mongooseUniqueValidator); // On associe le plugin "mongoose-unique-validator" pour qu'une vérification automatique de l'unicité du userId soit effectuée
 
